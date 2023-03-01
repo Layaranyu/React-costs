d0ddb23b3b08295937bab9b8e613886f0bd88d7a
Layaranyu <lay_nyu0501@outlook.com>
2023-03-01 10:10:43 -0300

    card adicionado

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,18 +1,57 @@
 import Message from "../Layout/Message"
 import {useLocation} from 'react-router-dom'
+import styles from './Projects.module.css'
+import Container from "../Layout/Container"
+import LinkButton from "../Layout/LinkButton"
+import { useState,useEffect } from "react"
+import ProjectCard from "../Project/ProjectCard"
+
 
 
 function Projects(){
+    const [projects,setProjects]=useState([])
+
     const location=useLocation()
     let message=''
     if(location.state){
         message =  location.state.message
     }
 
+    useEffect(()=>{
+        fetch('http://localhost:5000/Project',{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp)=>resp.json())
+            .then((data)=>{
+                console.log(data)
+                setProjects(data)
+            })
+            .catch((err)=> console.log(err))
+        },[])
+
+
+
+
     return (
-        <div>
-            <h1>Projetos </h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+            <h1>Meus Projetos </h1>
+            <LinkButton to='/newproject' text='Criar Projeto' />
+            </div>
             {message && <Message type="success" msg={message}/>}
+            <Container customClass="start">
+                {projects.length > 0 && 
+                projects.map((project)=>(
+                    <ProjectCard id={project.id}
+                     name={project.name}
+                     budget={project.budget}
+                     category={project?.category?.name}
+                     key={project.id}/>
+                ))}
+            </Container>
         </div>
     )
 }
